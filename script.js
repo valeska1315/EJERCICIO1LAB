d9481dd7a45c24ba1851b69ba384aa072b43c1ad
Valeska Garcia <70130772+valeska1315@users.noreply.github.com>
2021-03-08 17:41:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+VARIABLES Y TIPOS DE DATOS EN JAVA SCRIPT
+var saludo ="HOLA JacavScript"
+var edad = 26;
+var unpoco = 1.5;
+var SiNo = verdadero;
+var seccion = 'a';
+
+//MOSTRAR UN DATO EN CONSOLA
+consolo . log (saludo)
+
+var addEventListener = document.getElementById('favorito')
